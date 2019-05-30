4ce2b9aca94f7d812c8c77393daeb44814b819d0
Andrei243 <andrei243.nma@gmail.com>
2019-05-30 11:30:30 +0300

    Codul e pregatit pentru handshake dar nu stiu de ce nu se trimite deloc mesajul

--- src/tcp_handshake.py
+++ src/tcp_handshake.py
@@ -4,23 +4,51 @@
 from scapy.all import *
 
 ip = IP()
-ip.src = # ip-ul nostru
-ip.dst = # ip-ul serverului 
+ip.src ="172.11.0.14" # ip-ul nostru
+ip.dst ="198.12.0.14" # ip-ul serverului
+ip.show()
+
 tcp = TCP()
-tcp.sport = # un port la aleger
-tcp.dport = # portul destinatie pe care ruleaza serverul
-tcp.seq = # un sequence number la alegere
+tcp.sport =5000 # un port la aleger
+tcp.dport =5000 # portul destinatie pe care ruleaza serverul
+tcp.seq =50 # un sequence number la alegere
 tcp.flags = 'S'
+tcp.options=[('MSS',2)]
+tcp.show()
+
 
 SYN = ip/tcp
+SYN.show()
 raspuns_SYN_ACK = sr1(SYN)
 rasp_ack = raspuns_SYN_ACK.seq + 1
 rasp_seq = tcp.seq + 1
-
+print("Heya")
 tcp.seq = rasp_seq
 tcp.ack = rasp_ack
 tcp.flags = 'A'
 
 ACK= ip / tcp
-send (ACK)
+raspuns_ACK = sr1(ACK)
+data=[]
+data.append('a')
+data.append('b')
+data.append('c')
+data.append('def')
+for dat in data:
+    tcp.seq = tcp.seq + 1
+    tcp.ack = raspuns_ACK.seq + 1
+    mesaj = ip / tcp / dat
+    raspuns_ACK = sr1(mesaj)
+    raspuns_ACK.show()
+
+
+tcp.seq=tcp.seq+1
+tcp.ack=raspuns_ACK.seq+1
+tcp.flags='FIN'
+
+FIN=ip/tcp
+raspuns_FIN=sr1(FIN)
+
+
+
 
