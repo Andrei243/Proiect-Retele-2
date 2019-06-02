49e3e2d7d34a4d8f0c090083a940e73e0d0dd924
Andrei243 <andrei243.nma@gmail.com>
2019-06-02 13:42:29 +0300

    Aproape merge, tcpdump-ul vede handshake-ul, dar serverul inca nu ( am mutat serverul si clientul pe acelasi network)

--- src/tcp_handshake.py
+++ src/tcp_handshake.py
@@ -2,10 +2,11 @@
 # a pachetelor RST pe care ni le livreaza kernelul automat
 # iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
 from scapy.all import *
+import struct
 
 ip = IP()
 ip.src ="172.11.0.14" # ip-ul nostru
-ip.dst ="198.12.0.14" # ip-ul serverului
+ip.dst ="172.11.0.13" # ip-ul serverului
 ip.show()
 
 tcp = TCP()
@@ -13,7 +14,8 @@ tcp.sport =5000 # un port la aleger
 tcp.dport =5000 # portul destinatie pe care ruleaza serverul
 tcp.seq =50 # un sequence number la alegere
 tcp.flags = 'S'
-tcp.options=[('MSS',2)]
+valoare = struct.pack("!B",2)
+tcp.options=[('MSS',valoare)]
 tcp.show()
 
 
@@ -22,7 +24,6 @@ SYN.show()
 raspuns_SYN_ACK = sr1(SYN)
 rasp_ack = raspuns_SYN_ACK.seq + 1
 rasp_seq = tcp.seq + 1
-print("Heya")
 tcp.seq = rasp_seq
 tcp.ack = rasp_ack
 tcp.flags = 'A'
@@ -38,13 +39,12 @@ for dat in data:
     tcp.seq = tcp.seq + 1
     tcp.ack = raspuns_ACK.seq + 1
     mesaj = ip / tcp / dat
+    mesaj.show()
     raspuns_ACK = sr1(mesaj)
-    raspuns_ACK.show()
-
 
 tcp.seq=tcp.seq+1
 tcp.ack=raspuns_ACK.seq+1
-tcp.flags='FIN'
+tcp.flags='FA'
 
 FIN=ip/tcp
 raspuns_FIN=sr1(FIN)
--- src/tcp_server.py
+++ src/tcp_server.py
@@ -9,13 +9,15 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 5000
-adresa = 'localhost'
+adresa = '172.11.0.13'
 server_address = (adresa, port)
 sock.bind(server_address)
 logging.info("Serverul a pornit pe %s si portul %d", adresa, port)
 sock.listen(5)
-logging.info('Asteptam conexiui...')
+logging.info('Asteptam conexiuni...')
 conexiune, address = sock.accept()
+print("Am ajuns aici")
+
 logging.info("Handshake cu %s", address)
 while True:
 
